407ae5024cb9b976cfa44a9c1c0c6187106d6a69
suresh16s <sureshthite48@gmail.com>
2020-07-10 22:22:21 +0530

    UC_3

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -3,5 +3,7 @@ read -p "Enter the 1st number:" a
 read -p "Enter the 2nd number:" b
 read -p "Enter the 3rd number:" c
 
-ans_1=$(( (a+b)*c ))
-echo "The ans is :" $ans_1
+ans_2=$(( (a*b)+c ))
+echo "The ans is :" $ans_2
+
+
